805e46156c268bf20f27f07f184a8edec68ec825
LORDSON11 <lordsondavid.ei21@bitsathy.ac.in>
2025-06-25 11:23:44 +0530

    tf fixed

---
+++ devops/terraform/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region                  = "ap-south-1"
+  access_key              = var.aws_access_key
+  secret_key              = var.aws_secret_key
+}
+
+resource "aws_key_pair" "jenkins_key" {
+  key_name   = "jenkins-key"
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "allow_web_ssh" {
+  name        = "web-ssh"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami                    = "ami-0c768662cc797cd75"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.jenkins_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow_web_ssh.id]
+
+  tags = {
+    Name = "JenkinsDeployInstance"
+  }
+}
